3a1b2cb29c74c01ccd379843ba9a0cb6274812c4
EdmundGift <amingaedmund@gmail.com>
2024-02-29 22:40:34 +0300

    Add files via upload

---
+++ function.dart
@@ -0,0 +1,32 @@
+// Task 1
+int addTwo(int a, int b) {
+  return a + b;
+}
+
+// Task 2
+int subtractTwo(int a, int b) {
+  return a - b;
+}
+
+// Task 3
+int multiplyTwo(int a, int b) {
+  return a * b;
+}
+
+// Task 4
+double divideTwo(double a, double b) {
+  return a / b;
+}
+
+// Task 5
+int stringLength(String str) {
+  return str.length;
+}
+
+// Task 6
+dynamic getFirstElement(List list) {
+  if (list.isNotEmpty) {
+    return list[0];
+  }
+  return null;
+}
